0fc291ab05c63bfb0560585c64cc893889b9ac16
VISHNUPRASHOB <vishnuprashob07@gmail.com>
2023-05-07 11:38:15 +0530

    success updating file data

--- src/app/app-sim-img/app-sim-img.component.ts
+++ src/app/app-sim-img/app-sim-img.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FIleSchema } from '../app-models';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import {HttpClient, HttpHeaders} from '@angular/common/http';
+import { Observable } from 'rxjs';
 @Component({
   selector: 'app-app-sim-img',
   templateUrl: './app-sim-img.component.html',
@@ -28,7 +29,7 @@ export class AppSimImgComponent implements OnInit {
    apiToken =
   'h2yMCGyvkuFk1yVedt6bl9ukSFP88jD6BkEHtJQatY2midcZ7MLJp3um+m9r/lYHYEz++5U6R9JSOh4L4GAuopf4DW7oAzESKhcEShhQ983JKmXg7uWghzNuW+p4wfvVqsMcrr5RkmMHu3CiZDo9PQ==';
    url = "https://apihub.document360.net/v2/Drive/Drive?DriveFolderId=fc73fa23-0c36-490b-84b4-565574dd6377&BlobType=2&UserId=f11efc6f-e968-4e95-82eb-85ad61559de8";
-   
+   response2!: Observable<Object>;
      constructor(public fb: FormBuilder) {}   
   ngOnInit() {  
     this.headers2.append('api_token',this.apiToken );
@@ -44,8 +45,8 @@ uploadFile(event: Event, fileType: string) {
 submitForm(event: Event) {   
   const formData : any = new FormData();
  
-  formData.append('files', new Blob([JSON.stringify([event.target as HTMLInputElement | null][0]?.files?.item(0)?.arrayBuffer.toString())], {type:  this.showEvent?.type}), this.showEvent?.name);
-  this.res = [event.target as HTMLInputElement | null][0]?.files?.item(0)?.name;
+  formData.append('files', this.showEvent, this.showEvent?.name);
+  this.res = this.showEvent?.name;
   const apiToken =
   'h2yMCGyvkuFk1yVedt6bl9ukSFP88jD6BkEHtJQatY2midcZ7MLJp3um+m9r/lYHYEz++5U6R9JSOh4L4GAuopf4DW7oAzESKhcEShhQ983JKmXg7uWghzNuW+p4wfvVqsMcrr5RkmMHu3CiZDo9PQ==';
   const headers = new  Headers();  
@@ -61,8 +62,9 @@ const response =  fetch(
   options
 );
 console.log(response);
-const response2 = this.http.post(this.url,formData,{headers:this.headers2});
-console.log(response);
+
+//  this.response2 = this.http.post(this.url,formData,{headers:this.headers2});
+// console.log(this.response2);
 
 // Object.keys(this.form.controls).forEach(formControlName => {        
 //    formData.append(formControlName,  this.form.get(formControlName)?.value);    
